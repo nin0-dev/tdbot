04447faf1ba5d10340d8d2b57c5e1954d5dfc5d5
nin0dev <personal@nin0.dev>
2025-01-26 10:02:59 -0500

    add the insanity

---
+++ src/commands/kys.ts
@@ -0,0 +1,26 @@
+import { ApplicationCommandOptionTypes } from "oceanic.js";
+import { Command } from "../../utils/types";
+
+const command: Command = {
+	name: "kys",
+	description: "requested by vending.machine",
+	ownerOnly: true,
+	trustedOnly: false,
+	args: [
+		{
+			type: ApplicationCommandOptionTypes.USER,
+			name: "target",
+			description: "person to target",
+			required: true
+		}
+	],
+	execute: async (int, options) => {
+		int.reply({
+			content: `${options.getUser("target", true).mention} kys`,
+			allowedMentions: {
+				users: true
+			}
+		});
+	}
+};
+export default command;
